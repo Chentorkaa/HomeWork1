2a51e62fed8eefd1fa6d31fa1f31fa5df9c97053
Chentorka <chentorka@gmail.com>
2022-10-19 12:15:01 +0300

    Save example6

---
+++ Example6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите значение ");
+int Number1 = int.Parse(Console.ReadLine());
+
+if (Number1%2==0)
+{ 
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
